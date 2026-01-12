0a626d32517565b8cbc1cae9eb4b7d7d50f82328
sneha-yadav786 <sneha.7864786@gmail.com>
2026-01-12 22:57:32 +0530

    add to do list code

--- i.py
+++ i.py
@@ -1,20 +1,18 @@
 class Solution:
-    def countTrapezoids(self, points):
-        y_points={}
-        for point in points:
-            if point[1] in y_points:
-                y_points[point[1]]+=1
-            else:
-                y_points[point[1]]=1
-        print(y_points)
-        sum=1
-        for i in y_points:
-            if y_points[i]>1:
-                k=(y_points[i]*(y_points[i]-1))//2
-                sum*=k
-        return sum
+    def maximumHappinessSum(self, happiness, k):
+        happiness.sort()
+        s=0
+        print(happiness)
+        count=0
+        for i in range(k):
+            s+=happiness[-1]-count
+            t=happiness.pop()-count
+            print(t)
+            count+=1
+        return s
+
 
         
-        
 s1=Solution()
-print(s1.countTrapezoids([[50,-41],[64,-66],[-45,-41],[-58,10],[25,10]]))
+print(s1.maximumHappinessSum([12,1,42],3))
+
